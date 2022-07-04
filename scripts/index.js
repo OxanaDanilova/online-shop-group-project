51027899104e5c2e3a39cd5a667cfbb982653066
Holger <holgeraliusklose@gmail.com>
2022-07-04 14:26:34 +0200

    renderProductlist

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
-import { renderProductList } from './products'
-import { renderCart } from './cart'
+import { renderProductList } from "./products.js";
+//import { renderCart } from './cart'
 
-renderProductList()
-renderCart()
+renderProductList();
+//renderCart()
